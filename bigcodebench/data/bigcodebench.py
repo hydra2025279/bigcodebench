c61aa740beb58fed7222c0fea85c1265027ab7d6
Minh Le-Anh <155245967+AnhMinh-Le@users.noreply.github.com>
2025-11-09 16:42:36 +0700

    Update bigcodebench.py

--- bigcodebench/data/bigcodebench.py
+++ bigcodebench/data/bigcodebench.py
@@ -12,9 +12,9 @@ from bigcodebench.data.utils import (
 )
 from datasets import load_dataset
 
-BIGCODEBENCH_OVERRIDE_PATH = "/Users/brian/Documents/BKAI/RepoCodeGen/crosscoder/benchmark/bigcodebench/bigcodebench.jsonl"
+BIGCODEBENCH_OVERRIDE_PATH = os.environ.get("BIGCODEBENCH_OVERRIDE_PATH", None)
 BIGCODEBENCH_HF = "bigcode/bigcodebench"
-BIGCODEBENCH_VERSION = "v0.1.0"
+BIGCODEBENCH_VERSION = "v0.1.4"
 
 def _ready_bigcodebench_path(subset="full", version="default") -> str:
     if BIGCODEBENCH_OVERRIDE_PATH:
